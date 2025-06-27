89d7ae6f3d4fcd2787cbb3721891bce0b88e9292
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 21:14:25 +0530

    Updated code files via regeneration: DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -33,7 +33,7 @@ const DashboardPage = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
 
       <main className="flex-1 flex items-center justify-center p-4 sm:p-6 lg:p-8">
